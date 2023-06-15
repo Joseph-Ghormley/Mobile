ddcb5bc38c67aa260346cca62500ecda70e53d08
Joseph-Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2023-06-15 17:08:52 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+/*
+Made by Joseph Ghormley
+ */
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Mobile mobile1 = new Mobile("Nokia", "Win8", "Lumia", 10000);
+        Mobile mobile2 = new Mobile("Nokia", "Win8");
+
+        Android android1 = new Android("Samsung","Android", "Grand", 30000 );
+        Android android2 = new Android("Samsung", "Android");
+
+        BlackBerry blackBerry1 = new BlackBerry("BlackBerry", "RIM", "Curve", 20000);
+        BlackBerry blackBerry2 = new BlackBerry("BlackBerry", "RIM");
+
+        System.out.println(mobile1);
+        System.out.println(mobile2);
+        System.out.println(android1);
+        System.out.println(android2);
+        System.out.println(blackBerry1);
+        System.out.println(blackBerry2);
+    }
 }
