3372ca866412c687822069319315939e62978ff5
Joseph-Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2023-06-15 17:08:38 -0400

    Add files via upload

--- src/Android.java
+++ src/Android.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Android {
+import java.lang.String;
+public class Android extends Mobile
+{
+    public Android(String manufacture, String operating_system, String model, double cost)
+    {
+        super(manufacture, operating_system, model, cost);
+    }
+
+    public Android(String manufacture, String operating_system) {
+        super(manufacture, operating_system);
+    }
 }
