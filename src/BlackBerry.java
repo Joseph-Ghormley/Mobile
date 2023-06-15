0957377f78f8d12770be218ef9fb4b6d61521c0e
Joseph-Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2023-06-15 17:06:00 -0400

    Add files via upload

--- src/BlackBerry.java
+++ src/BlackBerry.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class BlackBerry {
+import java.lang.String;
+public class BlackBerry extends Mobile
+{
+    public BlackBerry(String manufacture, String operating_system, String model, double cost)
+    {
+        super(manufacture, operating_system, model, cost);
+    }
+    public BlackBerry(String manufacture, String operating_system)
+    {
+        super(manufacture, operating_system);
+    }
+
+
+
+
 }
