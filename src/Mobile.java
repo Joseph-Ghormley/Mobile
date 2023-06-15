3f1c6de0030e5838f3276a370b34ec1250d6f4d9
Joseph-Ghormley <54871175+Joseph-Ghormley@users.noreply.github.com>
2023-06-15 17:07:09 -0400

    Add files via upload

--- src/Mobile.java
+++ src/Mobile.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Mobile {
+import java.lang.String;
+class Mobile {
+    private String manufacture,operating_system,model;
+    private double cost;
+
+    public Mobile(String manufacture, String operating_system, String model, double cost) {
+        this.manufacture = manufacture;
+        this.operating_system = operating_system;
+        this.model = model;
+        this.cost = cost;
+    }
+
+    public Mobile(String manufacture, String operating_system) {
+        this.manufacture = manufacture;
+        this.operating_system = operating_system;
+    }
+
+    @Override
+    public String toString() {
+        return "Manufacture: " + manufacture +
+                "\nOperating System: " + operating_system +
+                "\nModel: " + model +
+                "\nCost: $" + cost;
+    }
 }
+
